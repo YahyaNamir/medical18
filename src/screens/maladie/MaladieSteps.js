6afc52c7dae523df40649bf58205574f246d97fc
YAHYA NAMIR <132499945+YahyaNamir@users.noreply.github.com>
2024-09-10 15:26:25 +0100

    Form append

--- src/screens/maladie/MaladieSteps.js
+++ src/screens/maladie/MaladieSteps.js
@@ -86,7 +86,7 @@ export default function MaladieSteps({navigation, route}) {
       //! ____________________
       form.append('bilans', formData.pageBilan.selectedBilans.join(','));
       form.append('refs', formData.pageBilan.selectedRefs.join(','));
-      form.append('reference_comment', formData.pageBilan.reference_comment);
+      // form.append('reference_comment', formData.pageBilan.reference_comment);
       form.append('bilan_comment', formData.pageBilan.bilan_comment);
       //! ____________________
 
